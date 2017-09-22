0af18c6e74329d5dc76464039323af3f2fd45810
Oliver Shi <oys4cv@virginia.edu>
2017-09-21 21:01:31 -0400

    started routing
    added router

--- oli.js
+++ oli.js
@@ -2,5 +2,18 @@
 	'use strict' //ECMA5 strict mode
 	var baseUrl = '/';
 	var oli = angular.module('Oli',['ngRoute']);
+	oli.config(function($routeProvider, $locationProvider, $httpProvider){
 
+		$locationProvider.html5Mode(true);
+
+
+		$routeProvider.when(baseUrl, {
+			templateUrl: baseUrl + 'components/homepage.html',
+			controllerAs: 'ctrl',
+			controller: function($rootScope){
+				var ctrl = this;
+				$rootScope.pageTitle = ' - Home';
+			}
+		});
+	})
 }(window.angular))
